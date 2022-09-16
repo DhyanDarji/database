39ecac5c50a1e40d5e932a8bf859278841851e0e
DhyanDarji <52266498+DhyanDarji@users.noreply.github.com>
2022-09-16 23:37:22 +0930

    Update Database.c

--- Database.c
+++ Database.c
@@ -55,6 +55,10 @@ int main(void) {
     // loop through the file and input into the structure.
     while (feof(file) == 0) {
 
+        //Followed the following guide to help me scan data from the file into structs
+        //https://www.youtube.com/watch?v=QrKpqdBiFRI&ab_channel=CodeVault
+        
+        
         //scan from the file into the structure.
         fscanf(file, inputFormat, person[filecount].FullName, person[filecount].studentID, &person[filecount].MemYear);
 
